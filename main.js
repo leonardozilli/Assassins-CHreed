497271a01e78be6776f29e91c9af8b7c3575775d
leonardozilli <leonardo.zilli19@gmail.com>
2023-05-01 16:23:05 +0200

    added last sections

--- main.js
+++ main.js
@@ -120,7 +120,6 @@ function zoom() {
 
       function setTransform() {
         zoom.style.transform = "translate(" + pointX + "px, " + pointY + "px) scale(" + scale + ")";
-        console.log(scale)
       }
 
       zoomcontainer.onmousedown = function (e) {
@@ -153,8 +152,147 @@ function zoom() {
         pointY = e.clientY - ys * scale;
 
         setTransform();
+        console.log(pointX, pointY)
       }
 
+}
+
+function zoom2() {
+        var scale = 1,
+        panning = false,
+        pointX = 0,
+        pointY = 0,
+        start = { x: 0, y: 0 },
+        zoom2 = document.getElementById("zoom2");
+        zoomcontainer2 = document.getElementById("zoomcontainer2");
+
+      function setTransform() {
+        zoom2.style.transform = "translate(" + pointX + "px, " + pointY + "px) scale(" + scale + ")";
+      }
+
+      zoomcontainer2.onmousedown = function (e) {
+        e.preventDefault();
+        start = { x: e.clientX - pointX, y: e.clientY - pointY };
+        panning = true;
+      }
+
+      zoomcontainer2.onmouseup = function (e) {
+        panning = false;
+      }
+
+      zoomcontainer2.onmousemove = function (e) {
+        e.preventDefault();
+        if (!panning) {
+          return;
+        }
+        pointX = (e.clientX - start.x);
+        pointY = (e.clientY - start.y);
+        setTransform();
+      }
+
+      zoomcontainer2.onwheel = function (e) {
+        e.preventDefault();
+        var xs = (e.clientX - pointX) / scale,
+          ys = (e.clientY - pointY) / scale,
+          delta = (e.wheelDelta ? e.wheelDelta : -e.deltaY);
+        (delta > 0) ? (scale *= 1.2) : (scale /= 1.2);
+        pointX = e.clientX - xs * scale;
+        pointY = e.clientY - ys * scale;
+
+        setTransform();
+      }
+}
+
+function zoom3() {
+        var scale = 1,
+        panning = false,
+        pointX = 0,
+        pointY = 0,
+        start = { x: 0, y: 0 },
+        zoom3 = document.getElementById("zoom3");
+        zoomcontainer2 = document.getElementById("zoomcontainer3");
+
+      function setTransform() {
+        zoom3.style.transform = "translate(" + pointX + "px, " + pointY + "px) scale(" + scale + ")";
+      }
+
+      zoomcontainer3.onmousedown = function (e) {
+        e.preventDefault();
+        start = { x: e.clientX - pointX, y: e.clientY - pointY };
+        panning = true;
+      }
+
+      zoomcontainer3.onmouseup = function (e) {
+        panning = false;
+      }
+
+      zoomcontainer3.onmousemove = function (e) {
+        e.preventDefault();
+        if (!panning) {
+          return;
+        }
+        pointX = (e.clientX - start.x);
+        pointY = (e.clientY - start.y);
+        setTransform();
+      }
+
+      zoomcontainer3.onwheel = function (e) {
+        e.preventDefault();
+        var xs = (e.clientX - pointX) / scale,
+          ys = (e.clientY - pointY) / scale,
+          delta = (e.wheelDelta ? e.wheelDelta : -e.deltaY);
+        (delta > 0) ? (scale *= 1.2) : (scale /= 1.2);
+        pointX = e.clientX - xs * scale;
+        pointY = e.clientY - ys * scale;
+
+        setTransform();
+      }
+}
+
+function zoom4() {
+        var scale = 1,
+        panning = false,
+        pointX = 0,
+        pointY = 0,
+        start = { x: 0, y: 0 },
+        zoom4 = document.getElementById("zoom4");
+        zoomcontainer4 = document.getElementById("zoomcontainer4");
+
+      function setTransform() {
+        zoom4.style.transform = "translate(" + pointX + "px, " + pointY + "px) scale(" + scale + ")";
+      }
+
+      zoomcontainer4.onmousedown = function (e) {
+        e.preventDefault();
+        start = { x: e.clientX - pointX, y: e.clientY - pointY };
+        panning = true;
+      }
+
+      zoomcontainer4.onmouseup = function (e) {
+        panning = false;
+      }
+
+      zoomcontainer4.onmousemove = function (e) {
+        e.preventDefault();
+        if (!panning) {
+          return;
+        }
+        pointX = (e.clientX - start.x);
+        pointY = (e.clientY - start.y);
+        setTransform();
+      }
+
+      zoomcontainer4.onwheel = function (e) {
+        e.preventDefault();
+        var xs = (e.clientX - pointX) / scale,
+          ys = (e.clientY - pointY) / scale,
+          delta = (e.wheelDelta ? e.wheelDelta : -e.deltaY);
+        (delta > 0) ? (scale *= 1.2) : (scale /= 1.2);
+        pointX = e.clientX - xs * scale;
+        pointY = e.clientY - ys * scale;
+
+        setTransform();
+      }
 }
 
 function carousel() {
@@ -241,11 +379,18 @@ function carousel() {
 
 
 function selectTab(){
-    griditems = document.getElementsByClassName('gridtab')
+    griditems1 = document.getElementsByClassName('gridtab1')
+    griditems2 = document.getElementsByClassName('gridtab2')
     cmtab = document.getElementById('conceptualmaptab')
     ertab = document.getElementById('ermodeltab')
     erbut = document.getElementById('ermodelbutton')
     cmbut = document.getElementById('conceptualmapbutton')
+
+    tmtab = document.getElementById('theoreticalmodeltab')
+    eertab = document.getElementById('enhancedermodeltab')
+    eerbut = document.getElementById('enhancederbutton')
+    tmbut = document.getElementById('theoreticalmodelbutton')
+
     whoTab = document.getElementById('whoTab')
     whenTab = document.getElementById('whenTab')
     whereTab = document.getElementById('whereTab')
@@ -276,16 +421,17 @@ function selectTab(){
         whatTab.classList.add('selected')
         document.getElementById('whatTable').classList.remove('hidden');
     });
+
     erbut.addEventListener("click", function (e) {
         document.getElementById('ermodel').classList.remove('hidden');
         document.getElementById('conceptualmap').classList.add('hidden');
-        Array.from(griditems).forEach(griditem => griditem.classList.remove('active'));
+        Array.from(griditems1).forEach(griditem => griditem.classList.remove('active'));
         ertab.classList.add('active')
     });
     cmbut.addEventListener("click", function (e) {
         document.getElementById('conceptualmap').classList.remove('hidden')
         document.getElementById('ermodel').classList.add('hidden')
-        Array.from(griditems).forEach(griditem => griditem.classList.remove('active'));
+        Array.from(griditems1).forEach(griditem => griditem.classList.remove('active'));
         cmtab.classList.add('active')
     });
     cmtab.addEventListener("click", function (e) {
@@ -296,10 +442,39 @@ function selectTab(){
         document.getElementById('ermodel').classList.remove('hidden')
         document.getElementById('conceptualmap').classList.add('hidden')
     });
-    for (let i = 0; i < griditems.length; i++) {
-        griditems[i].addEventListener("click", function (e) { 
-            Array.from(griditems).forEach(griditem => griditem.classList.remove('active'));
-            griditems[i].classList.add('active')
+
+    eerbut.addEventListener("click", function (e) {
+        document.getElementById('enhancedermodel').classList.remove('hidden');
+        document.getElementById('theoreticalmodel').classList.add('hidden');
+        Array.from(griditems2).forEach(griditem => griditem.classList.remove('active'));
+        eertab.classList.add('active')
+    });
+    tmbut.addEventListener("click", function (e) {
+        document.getElementById('theoreticalmodel').classList.remove('hidden')
+        document.getElementById('enhancedermodel').classList.add('hidden')
+        Array.from(griditems2).forEach(griditem => griditem.classList.remove('active'));
+        tmtab.classList.add('active')
+    });
+    tmtab.addEventListener("click", function (e) {
+        document.getElementById('theoreticalmodel').classList.remove('hidden')
+        document.getElementById('enhancedermodel').classList.add('hidden')
+    });
+    eertab.addEventListener("click", function (e) {
+        document.getElementById('enhancedermodel').classList.remove('hidden')
+        document.getElementById('theoreticalmodel').classList.add('hidden')
+    });
+
+    for (let i = 0; i < griditems1.length; i++) {
+        griditems1[i].addEventListener("click", function (e) { 
+            Array.from(griditems1).forEach(griditem => griditem.classList.remove('active'));
+            griditems1[i].classList.add('active')
+        });
+    }
+
+    for (let i = 0; i < griditems2.length; i++) {
+        griditems2[i].addEventListener("click", function (e) { 
+            Array.from(griditems2).forEach(griditem => griditem.classList.remove('active'));
+            griditems2[i].classList.add('active')
         });
     }
 }
@@ -326,6 +501,9 @@ $(document).ready(function () {
 
     carousel();
     zoom();
+    zoom2();
+    zoom3();
+    zoom4();
     selectTab();
 
 
